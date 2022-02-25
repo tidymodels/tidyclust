c04601a20c6f45ff842d6e874c778aa443147bac
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-02-24 19:43:57 -0800

    add ClusterR k_means

--- R/k_means_data.R
+++ R/k_means_data.R
@@ -39,3 +39,43 @@ set_model_arg_celery(
   func = list(pkg = "dials", fun = "k"),
   has_submodel = TRUE
 )
+
+# ------------------------------------------------------------------------------
+
+set_model_engine_celery("k_means", "partition", "ClusterR")
+set_dependency_celery("k_means", "ClusterR", "ClusterR")
+
+set_fit_celery(
+  model = "k_means",
+  eng = "ClusterR",
+  mode = "partition",
+  value = list(
+    interface = "matrix",
+    data = c(x = "data"),
+    protect = c("data", "clusters"),
+    func = c(pkg = "ClusterR", fun = "KMeans_rcpp"),
+    defaults = list()
+  )
+)
+
+set_encoding_celery(
+  model = "k_means",
+  eng = "ClusterR",
+  mode = "partition",
+  options = list(
+    predictor_indicators = "traditional",
+    compute_intercept = TRUE,
+    remove_intercept = TRUE,
+    allow_sparse_x = FALSE
+  )
+)
+
+set_model_arg_celery(
+  model = "k_means",
+  eng = "ClusterR",
+  celery = "k",
+  original = "clusters",
+  func = list(pkg = "dials", fun = "k"),
+  has_submodel = TRUE
+)
+
