21d5b2da3bad31573d14724cc040f58dbae84683
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-03-12 21:33:56 -0800

    add printing methods for cluster_spec and cluster_fit

---
+++ R/print.R
@@ -0,0 +1,26 @@
+#' @export
+print.cluster_fit <- function(x, ...) {
+  cat("celery cluster object\n\n")
+  if (!is.na(x$elapsed[["elapsed"]])) {
+    cat("Fit time: ", prettyunits::pretty_sec(x$elapsed[["elapsed"]]),
+        "\n")
+  }
+  if (inherits(x$fit, "try-error")) {
+    cat("Cluster fit failed with error:\n", x$fit, "\n")
+  }
+  else {
+    print(x$fit, ...)
+  }
+  invisible(x)
+}
+
+#' @export
+print.cluster_spec <- function(x, ...) {
+  cat("Cluster Specification (", x$mode, ")\n\n", sep = "")
+  model_printer(x, ...)
+  if (!is.null(x$method$fit$args)) {
+    cat("Cluster fit template:\n")
+    print(show_call(x))
+  }
+  invisible(x)
+}
