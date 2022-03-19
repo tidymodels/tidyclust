375de4a1e4e7406a482fb41df17f0b115c6c318e
Kelly N. Bodwin <kelly@bodwin.us>
2022-03-19 00:26:16 -0400

    working on cluster renames, doesn't quite work right yet

--- R/predict_helpers.R
+++ R/predict_helpers.R
@@ -1,9 +1,12 @@
 stats_kmeans_predict <- function(object, new_data) {
-  res <- apply(flexclust::dist2(object$centers, new_data), 2, which.min)
-  factor(res, levels = seq_along(object$size))
+  clusters <- apply(flexclust::dist2(object$centers, new_data), 2, which.min)
+
 }
 
 clusterR_kmeans_predict <- function(object, new_data) {
   res <- apply(flexclust::dist2(object$centroids, new_data), 2, which.min)
   factor(res, levels = seq_along(object$obs_per_cluster))
 }
+
+
+### I need the original cluster labels to line up with the new ones
