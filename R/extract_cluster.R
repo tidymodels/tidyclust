68971fc01a6829075bb58943998d4a15a955017b
Kelly N. Bodwin <kelly@bodwin.us>
2022-03-25 12:11:35 -0700

    Use summary extraction as method, then de-method-ifize centroid extraction

--- R/extract_cluster.R
+++ R/extract_cluster.R
@@ -14,28 +14,7 @@
 #'   extract_clusters()
 #' @export
 extract_clusters <- function(object, ...) {
-  UseMethod("extract_clusters")
-}
-
-#' @export
-extract_clusters.cluster_fit <- function(object, ...) {
-  extract_clusters(object$fit)
-}
-
-#' @export
-extract_clusters.kmeans <- function(object, ...) {
-  clusters_tibble(object$centers)
-}
-
-#' @export
-extract_clusters.KMeansCluster <- function(object, ...) {
-  clusters_tibble(object$centroids)
-}
-
-# ------------------------------------------------------------------------------
-
-clusters_tibble <- function(x) {
-  clusters <- tibble::tibble(.cluster = factor(seq_len(nrow(x))))
-
-  bind_cols(clusters, x)
+  summ <- extract_fit_summary(object)
+  clusters <- tibble::tibble(.cluster = summ$cluster_names)
+  bind_cols(clusters, summ$centroids)
 }
