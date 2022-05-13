b3b6b7d41ac9e903852036d0f3c54650cfc88b77
Kelly N. Bodwin <kelly@bodwin.us>
2022-05-12 23:07:36 -0700

    meh cross val doesn't work

--- dev/cross_val_kmeans.R
+++ dev/cross_val_kmeans.R
@@ -52,5 +52,49 @@ res %>%
 ### Second idea
 ## What if we cluster the whole data, then see how well subsamples are reclassified?
 ## This needs "predict"
+## Doesn't really make sense yet
+
+cvs <- vfold_cv(ir, v = 5)
+
+res <- data.frame(
+  k = NA,
+  i = NA,
+  acc = NA,
+  roc_auc = NA
+)
+
+for (k in 2:10) {
+
+  km <- k_means(k = k) %>%
+    set_engine_celery("stats")
+
+  full_fit <- km %>% fit(~., data = ir) %>%
+    extract_cluster_assignment()
 
 
+  for (i in 1:5) {
+
+    tmp_train <- training(cvs$splits[[i]])
+    tmp_test <- testing(cvs$splits[[i]])
+
+    km_fit <- km %>% fit(~., data = tmp_train)
+
+    dat <- tmp_train %>%
+      mutate(
+        truth = full_fit$.cluster[as.numeric(rownames(tmp_train))],
+        estimate = extract_cluster_assignment(km_fit)$.cluster
+      )
+
+    acc <- accuracy(dat, truth, estimate)
+
+    res <- rbind(res,
+                 c(k = k, i = i, acc = acc$.estimate[1], roc_auc = NA))
+
+  }
+
+}
+
+
+res %>%
+  ggplot(aes(x = factor(k), y = acc)) +
+  geom_boxplot()
