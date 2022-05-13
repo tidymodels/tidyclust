10c63beccbdfb934f6d9d1d4e385079720ace370
Kelly N. Bodwin <kelly@bodwin.us>
2022-05-12 23:51:54 -0700

    the hungarian algorithm is cool.

---
+++ R/reconcile_clusterings.R
@@ -0,0 +1,29 @@
+library(RcppHungarian)
+
+reconcile_clusterings <- function(clusters_1, clusters_2) {
+
+  clusters_1 <- factor(clusters_1)
+  clusters_2 <- factor(clusters_2)
+
+  cost <- as.matrix(table(clusters_1, clusters_2))
+  matches <- HungarianSolver(-cost)
+
+  c1_names <- unique(clusters_1)
+  c2_new <- factor(clusters_2,
+                   levels = levels(clusters_2)[matches$pairs[,2]],
+                   labels = levels(clusters_1))
+
+  res <- tibble::tibble(
+    clusters_1 = clusters_1,
+    clusters_2 = clusters_2,
+    clusters_2_renamed = c2_new
+  )
+
+  res
+
+}
+
+clusters_1 <- c("C1", "C1", "C2", "C3", "C4", "C4")
+clusters_2 <- c("C3", "C2", "C2", "C4", "C1", "C1")
+
+reconcile_clusterings(clusters_1, clusters_2)
