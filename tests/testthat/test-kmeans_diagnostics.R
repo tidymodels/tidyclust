a74dd2405c41bc8eb7842379b4400187c9ad3dcc
Kelly N. Bodwin <kelly@bodwin.us>
2022-05-26 21:27:25 -0700

    fix kmeans diagnostics tests

--- tests/testthat/test-kmeans_diagnostics.R
+++ tests/testthat/test-kmeans_diagnostics.R
@@ -7,38 +7,39 @@ obj2 <- k_means(k = 3) %>%
   fit(~., mtcars)
 
 
+km_orig <- kmeans(mtcars, centers = mtcars[1:3,], algorithm = "MacQueen")
+km_orig_2 <- ClusterR::KMeans_rcpp(mtcars, clusters = 3, CENTROIDS = as.matrix(mtcars[1:3,]))
+
 new_data <- mtcars[1:4,]
 
 test_that("kmeans sse metrics work", {
 
-  expect_equal(within_cluster_sse(obj1)$sse,
-               c(42877.103, 76954.010, 7654.146),
+  expect_equal(within_cluster_sse(obj1)$wss,
+               c(42877.103, 76954.010, 7654.146), # hard coded because of order
                tolerance = 0.005)
 
-  expect_equal(tot_wss(obj1), 127485.3, tolerance = 0.005)
-  expect_equal(tot_sse(obj1), 623387.5, tolerance = 0.005)
-  expect_equal(sse_ratio(obj1), 0.204504, tolerance = 0.005)
+  expect_equal(tot_wss(obj1), km_orig$tot.withinss, tolerance = 0.005)
+  expect_equal(tot_sse(obj1), km_orig$totss, tolerance = 0.005)
+  expect_equal(sse_ratio(obj1), km_orig$tot.withinss/km_orig$totss, tolerance = 0.005)
 
-
-  expect_equal(within_cluster_sse(obj2)$sse,
-               c(56041.432, 4665.041, 42877.103),
+  expect_equal(within_cluster_sse(obj2)$wss,
+               c(56041.432, 4665.041, 42877.103),  # hard coded because of order
                tolerance = 0.005)
 
-  expect_equal(tot_wss(obj2), 103583.6, tolerance = 0.005)
+  expect_equal(tot_wss(obj2), sum(km_orig_2$WCSS_per_cluster), tolerance = 0.005)
   expect_equal(tot_sse(obj2), tot_sse(obj1), tolerance = 0.005)
-  expect_equal(sse_ratio(obj2), 0.1661624, tolerance = 0.005)
 
 })
 
 test_that("kmeans sse metrics work on new data", {
 
-  expect_equal(within_cluster_sse(obj1, new_data)$sse,
-               c(933.0699, 12855.1696),
+  expect_equal(within_cluster_sse(obj1, new_data)$wss,
+               c(2799.21, 12855.17),
                tolerance = 0.005)
 
-  expect_equal(tot_wss(obj1, new_data), 13788.24, tolerance = 0.005)
+  expect_equal(tot_wss(obj1, new_data), 15654.38, tolerance = 0.005)
   expect_equal(tot_sse(obj1, new_data), 32763.7, tolerance = 0.005)
-  expect_equal(sse_ratio(obj1, new_data), 0.4208389, tolerance = 0.005)
+  expect_equal(sse_ratio(obj1, new_data), 15654.38/32763.7, tolerance = 0.005)
 
 })
 
