aa42bd469cb7e05ee096f47cec50455838d4a450
Kelly N. Bodwin <kelly@bodwin.us>
2022-06-09 22:10:49 -0700

    prep predict

--- R/predict_helpers.R
+++ R/predict_helpers.R
@@ -11,3 +11,12 @@ clusterR_kmeans_predict <- function(object, new_data) {
   res <- paste0("Cluster_", res)
   factor(res)
 }
+
+stats_hier_clust_predict <- function(object, new_data) {
+  ### record joins of new data according to h
+  ### this means we need an h even if k was specified (min or max or half?)
+  ### min because if they joined above, then that cut would be more clusts
+
+  ### find closest from original data, using specified linkage
+
+}
