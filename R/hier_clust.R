f4c33c53b7e5d6abffeaece4d279611f0b69a575
Kelly N. Bodwin <kelly@bodwin.us>
2022-06-09 21:56:39 -0700

    hier clust specs

---
+++ R/hier_clust.R
@@ -0,0 +1,96 @@
+#' Hierarchical (Agglomerative) Clustering
+#'
+#' @description
+#'
+#' `hier_clust()` defines a model that fits clusters based on a distance-based
+#' dendrogram
+#'
+#' @param mode A single character string for the type of model.
+#'  The only possible value for this model is "partition".
+#' @param engine A single character string specifying what computational engine
+#'  to use for fitting. Possible engines are listed below. The default for this
+#'  model is `"stats"`.
+#' @param k Positive integer, number of clusters in model (optional).
+#' @param h Positive double, height at which to cut dendrogram to obtain cluster
+#' assignments (only used if `k` is `NULL`)
+#' @param method the agglomeration method to be used. This should be (an
+#' unambiguous abbreviation of) one of `"ward.D"`, `"ward.D2"`, `"single"`,
+#' `"complete"`, `"average"` (= UPGMA), `"mcquitty"` (= WPGMA), `"median"`
+#' (= WPGMC) or `"centroid"` (= UPGMC).
+#' @param dist_fun A distance function to use
+#'
+#' @examples
+#' # show_engines("hier_clust")
+#'
+#' hier_clust()
+#' @export
+hier_clust <-
+  function(mode = "partition",
+           engine = "stats",
+           k = NULL,
+           h = NULL,
+           method = "complete",
+           dist_fun = Rfast::dista) {
+    args <- list(
+      k = enquo(k),
+      h = enquo(h),
+      method = enquo(method),
+      dist_fun = enquo(dist_fun)
+    )
+
+    new_cluster_spec(
+      "hier_clust",
+      args = args,
+      eng_args = NULL,
+      mode = mode,
+      method = NULL,
+      engine = engine
+    )
+  }
+
+#' @export
+print.hier_clust <- function(x, ...) {
+  cat("Hierarchical Clustering Specification (", x$mode, ")\n\n", sep = "")
+  model_printer(x, ...)
+
+  if (!is.null(x$method$fit$args)) {
+    cat("Model fit template:\n")
+    print(show_call(x))
+  }
+
+  invisible(x)
+}
+
+#' @export
+translate_celery.hier_clust <- function(x, engine = x$engine, ...) {
+  x <- translate_celery.default(x, engine, ...)
+  x
+}
+
+# ------------------------------------------------------------------------------
+
+#' Simple Wrapper around hclust function
+#'
+#' This wrapper prepares the data into a distance matrix to send to
+#' `stats::hclust` and retains the parameters `k` or `h` as an attribute.
+#'
+#' @param data matrix or data frame
+#' @param k the number of clusters
+#' @param h the height to cut the dendrogram
+#' @param method the agglomeration method to be used. This should be (an
+#' unambiguous abbreviation of) one of `"ward.D"`, `"ward.D2"`, `"single"`,
+#' `"complete"`, `"average"` (= UPGMA), `"mcquitty"` (= WPGMA), `"median"`
+#' (= WPGMC) or `"centroid"` (= UPGMC).
+#' @param dist_fun A distance function to use
+#'
+#' @return A dendrogram
+#' @keywords internal
+#' @export
+hclust_fit <- function(data, k = NULL, h = NULL,
+                       method = "complete",
+                       dist_fun = Rfast::dista) {
+  dmat <- dist_fun(data)
+  res <- hclust(dmat, method = method)
+  attr(res, "k") <- k
+  attr(res, "h") <- h
+}
---
+++ R/hier_clust_data.R
@@ -0,0 +1,123 @@
+set_new_model_celery("hier_clust")
+
+set_model_mode_celery("hier_clust", "partition")
+
+# ------------------------------------------------------------------------------
+
+set_model_engine_celery("hier_clust", "partition", "stats")
+set_dependency_celery("hier_clust", "stats", "stats")
+
+set_fit_celery(
+  model = "hier_clust",
+  eng = "stats",
+  mode = "partition",
+  value = list(
+    interface = "matrix",
+    protect = c(),  #### what is this?
+    func = c(pkg = "celery", fun = "hclust_fit"),
+    defaults = list(method = "complete", dist_fun = RFast::dista)
+  )
+)
+
+set_encoding_celery(
+  model = "hier_clust",
+  eng = "stats",
+  mode = "partition",
+  options = list(
+    predictor_indicators = "traditional",
+    compute_intercept = TRUE,
+    remove_intercept = TRUE,
+    allow_sparse_x = FALSE
+  )
+)
+
+set_model_arg_celery(
+  model = "hier_clust",
+  eng = "stats",
+  celery = "k",
+  original = "k",
+  func = list(pkg = "celery", fun = "k"),
+  has_submodel = TRUE
+)
+
+set_model_arg_celery(
+  model = "hier_clust",
+  eng = "stats",
+  celery = "cut_height",
+  original = "h",
+  func = list(pkg = "celery", fun = "cut_height"),
+  has_submodel = TRUE
+)
+
+set_pred_celery(
+  model = "hier_clust",
+  eng = "stats",
+  mode = "partition",
+  type = "cluster",
+  value = list(
+    pre = NULL,
+    post = NULL,
+    func = c(fun = "stats_hclust_predict"),
+    args =
+      list(
+        object = rlang::expr(object$fit),
+        new_data = rlang::expr(new_data)
+      )
+  )
+)
+
+# ------------------------------------------------------------------------------
+#
+# set_model_engine_celery("k_means", "partition", "ClusterR")
+# set_dependency_celery("k_means", "ClusterR", "ClusterR")
+#
+# set_fit_celery(
+#   model = "k_means",
+#   eng = "ClusterR",
+#   mode = "partition",
+#   value = list(
+#     interface = "matrix",
+#     data = c(x = "data"),
+#     protect = c("data", "clusters"),
+#     func = c(pkg = "celery", fun = "ClusterR_kmeans_fit"),
+#     defaults = list()
+#   )
+# )
+#
+# set_encoding_celery(
+#   model = "k_means",
+#   eng = "ClusterR",
+#   mode = "partition",
+#   options = list(
+#     predictor_indicators = "traditional",
+#     compute_intercept = TRUE,
+#     remove_intercept = TRUE,
+#     allow_sparse_x = FALSE
+#   )
+# )
+#
+# set_model_arg_celery(
+#   model = "k_means",
+#   eng = "ClusterR",
+#   celery = "k",
+#   original = "clusters",
+#   func = list(pkg = "dials", fun = "k"),
+#   has_submodel = TRUE
+# )
+#
+# set_pred_celery(
+#   model = "k_means",
+#   eng = "ClusterR",
+#   mode = "partition",
+#   type = "cluster",
+#   value = list(
+#     pre = NULL,
+#     post = NULL,
+#     func = c(fun = "clusterR_kmeans_predict"),
+#     args =
+#       list(
+#         object = rlang::expr(object$fit),
+#         new_data = rlang::expr(new_data)
+#       )
+#   )
+# )
