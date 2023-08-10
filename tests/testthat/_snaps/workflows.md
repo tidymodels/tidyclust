2cce3a3be350746912bceb2cf19023f02164399d
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-08-10 13:29:29 -0700

    add tests for workflows integration

---
+++ tests/testthat/_snaps/workflows.md
@@ -0,0 +1,24 @@
+# integrates with workflows::add_variables()
+
+    Code
+      fit(wf_spec, data = mtcars)
+    Condition
+      Error in `x_x()`:
+      ! Outcomes are not used in `cluster_spec` objects.
+
+# integrates with workflows::add_formula()
+
+    Code
+      fit(wf_spec, data = mtcars)
+    Condition
+      Error in `x_x()`:
+      ! Outcomes are not used in `cluster_spec` objects.
+
+# integrates with workflows::add_recipe()
+
+    Code
+      fit(wf_spec, data = mtcars)
+    Condition
+      Error in `x_x()`:
+      ! Outcomes are not used in `cluster_spec` objects.
+
---
+++ tests/testthat/test-workflows.R
@@ -0,0 +1,69 @@
+test_that("integrates with workflows::add_variables()", {
+  skip_if_not_installed("workflows")
+
+  kmeans_spec <- k_means(num_clusters = 2)
+
+  wf_spec <- workflows::workflow() %>%
+    workflows::add_variables(outcomes = NULL, predictors = everything()) %>%
+    workflows::add_model(kmeans_spec)
+
+  expect_no_error(
+    fit(wf_spec, data = mtcars)
+  )
+
+  wf_spec <- workflows::workflow() %>%
+    workflows::add_variables(outcomes = mpg, predictors = everything()) %>%
+    workflows::add_model(kmeans_spec)
+
+  expect_snapshot(
+    error = TRUE,
+    fit(wf_spec, data = mtcars)
+  )
+})
+
+test_that("integrates with workflows::add_formula()", {
+  skip_if_not_installed("workflows")
+
+  kmeans_spec <- k_means(num_clusters = 2)
+
+  wf_spec <- workflows::workflow() %>%
+    workflows::add_formula(~ .) %>%
+    workflows::add_model(kmeans_spec)
+
+  expect_no_error(
+    fit(wf_spec, data = mtcars)
+  )
+
+  wf_spec <- workflows::workflow() %>%
+    workflows::add_formula(mpg ~ .) %>%
+    workflows::add_model(kmeans_spec)
+
+  expect_snapshot(
+    error = TRUE,
+    fit(wf_spec, data = mtcars)
+  )
+})
+
+test_that("integrates with workflows::add_recipe()", {
+  skip_if_not_installed("workflows")
+  skip_if_not_installed("recipes")
+
+  kmeans_spec <- k_means(num_clusters = 2)
+
+  wf_spec <- workflows::workflow() %>%
+    workflows::add_recipe(recipes::recipe(~ ., data = mtcars)) %>%
+    workflows::add_model(kmeans_spec)
+
+  expect_no_error(
+    fit(wf_spec, data = mtcars)
+  )
+
+  wf_spec <- workflows::workflow() %>%
+    workflows::add_recipe(recipes::recipe(mpg ~ ., data = mtcars)) %>%
+    workflows::add_model(kmeans_spec)
+
+  expect_snapshot(
+    error = TRUE,
+    fit(wf_spec, data = mtcars)
+  )
+})
