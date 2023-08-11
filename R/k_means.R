682ff334d737726a2553c99d3218631c3c5c258b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-08-11 16:34:22 -0700

    check for missing `clusters` in ClusterR_kmeans_fit

--- R/k_means.R
+++ R/k_means.R
@@ -155,6 +155,13 @@ ClusterR_kmeans_fit <- function(data, clusters, num_init = 1, max_iters = 100,
                                 initializer = "kmeans++", fuzzy = FALSE,
                                 verbose = FALSE, CENTROIDS = NULL, tol = 1e-04,
                                 tol_optimal_init = 0.3, seed = 1) {
+  if (is.null(clusters)) {
+    rlang::abort(
+      "Please specify `num_clust` to be able to fit specification.",
+      call = call("fit")
+    )
+  }
+
   res <- ClusterR::KMeans_rcpp(
     data,
     clusters,
@@ -190,5 +197,6 @@ stats_kmeans_fit <- function(data, centers = NULL, ...) {
       call = call("fit")
     )
   }
+
   stats::kmeans(data, centers, ...)
 }
