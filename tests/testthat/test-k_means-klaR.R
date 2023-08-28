27600dd0a5b5826f84b777b1ccc6cbd33a99f8e1
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-08-28 15:39:35 -0700

    add tests for k_means klaR

---
+++ tests/testthat/test-k_means-klaR.R
@@ -0,0 +1,135 @@
+test_that("fitting", {
+  skip_if_not_installed("klaR")
+  skip_if_not_installed("modeldata")
+
+  data("ames", package = "modeldata")
+
+  ames_cat <- dplyr::select(ames, dplyr::where(is.factor))
+
+  set.seed(1234)
+  spec <- k_means(num_clusters = 3) %>%
+    set_engine("klaR")
+
+  expect_no_error(
+    res <- fit(spec, ~., ames_cat)
+  )
+
+  expect_no_error(
+    res <- fit_xy(spec, ames_cat)
+  )
+
+  expect_identical(res$fit$weighted, FALSE)
+
+  spec <- k_means(num_clusters = 3) %>%
+    set_engine("klaR", weighted = TRUE)
+
+  res <- fit(spec, ~., ames_cat)
+
+  expect_identical(res$fit$weighted, TRUE)
+})
+
+test_that("predicting", {
+  skip_if_not_installed("klaR")
+  skip_if_not_installed("modeldata")
+
+  data("ames", package = "modeldata")
+
+  ames_cat <- dplyr::select(ames, dplyr::where(is.factor))
+
+  set.seed(1234)
+  spec <- k_means(num_clusters = 3) %>%
+    set_engine("klaR")
+
+  res <- fit(spec, ~., ames_cat)
+
+  preds <- predict(res, ames_cat[c(1:5), ])
+
+  expect_identical(
+    preds,
+    tibble::tibble(.pred_cluster = factor(paste0("Cluster_", c(2, 2, 2, 2, 3)),
+                                          paste0("Cluster_", 1:3)))
+  )
+})
+
+test_that("predicting ties argument works", {
+  skip_if_not_installed("klaR")
+
+  dat <- data.frame(
+    x = c("A", "A", "B", "B", "C"),
+    y = c("A", "A", "B", "B", "C")
+  )
+
+  set.seed(1234)
+  spec <- k_means(num_clusters = 2) %>%
+    set_engine("klaR")
+
+  res <- fit(spec, ~., dat)
+
+  expect_identical(
+    predict(res, data.frame(x = "C", y = "C"), ties = "first"),
+    tibble::tibble(.pred_cluster = factor("Cluster_1",
+                                          paste0("Cluster_", 1:2)))
+  )
+
+  expect_identical(
+    predict(res, data.frame(x = "C", y = "C"), ties = "last"),
+    tibble::tibble(.pred_cluster = factor("Cluster_2",
+                                          paste0("Cluster_", 1:2)))
+  )
+})
+
+test_that("extract_centroids() works", {
+  skip_if_not_installed("klaR")
+  skip_if_not_installed("modeldata")
+
+  data("ames", package = "modeldata")
+
+  ames_cat <- dplyr::select(ames, dplyr::where(is.factor))
+
+  set.seed(1234)
+  spec <- k_means(num_clusters = 3) %>%
+    set_engine("klaR")
+
+  res <- fit(spec, ~., ames_cat)
+
+  centroids <- extract_centroids(res)
+
+  expected <- vctrs::vec_cbind(
+    tibble::tibble(.cluster = factor(paste0("Cluster_", 1:3))),
+    tibble::as_tibble(res$fit$modes)[c(3, 1, 2), ]
+  )
+
+  expect_identical(
+    centroids,
+    expected
+  )
+})
+
+test_that("extract_cluster_assignment() works", {
+  skip_if_not_installed("klaR")
+  skip_if_not_installed("modeldata")
+
+  data("ames", package = "modeldata")
+
+  ames_cat <- dplyr::select(ames, dplyr::where(is.factor))
+
+  set.seed(1234)
+  spec <- k_means(num_clusters = 3) %>%
+    set_engine("klaR")
+
+  res <- fit(spec, ~., ames_cat)
+
+  clusters <- extract_cluster_assignment(res)
+
+  exp_cluster <- res$fit$cluster
+  exp_cluster <- order(unique(exp_cluster))[exp_cluster]
+
+  expected <- vctrs::vec_cbind(
+    tibble::tibble(.cluster = factor(paste0("Cluster_", exp_cluster)))
+  )
+
+  expect_identical(
+    clusters,
+    expected
+  )
+})
