14a66b47fa86036bf98daca236eb076e375570af
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-08-30 15:35:20 -0700

    document details_k_means_clustMixType

--- R/k_means.R
+++ R/k_means.R
@@ -13,6 +13,7 @@
 #' - \link[=details_k_means_stats]{stats}: Classical K-means
 #' - \link[=details_k_means_ClusterR]{ClusterR}: Classical K-means
 #' - \link[=details_k_means_klaR]{klaR}: K-Modes
+#' - \link[=details_k_means_clustMixType]{clustMixType}: K-prototypes
 #'
 #' @param mode A single character string for the type of model. The only
 #'   possible value for this model is "partition".
---
+++ R/k_means_clustMixType.R
@@ -0,0 +1,15 @@
+#' K-means via clustMixType
+#'
+#' [k_means()] creates K-prototypes model. A K-prototypes is the middle ground
+#' between a K-means and K-modes model, in the sense that it can be used with
+#' data that contains both numeric and categorical predictors.
+#'
+#' Both numeric and categorical predictors are requires for this engine.
+#'
+#' @includeRmd man/rmd/k_means_clustMixType.md details
+#'
+#' @name details_k_means_clustMixType
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed
