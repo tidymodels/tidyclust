022144027e485d941accb3410807093dff6dbe86
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-08-30 16:30:48 -0700

    document details_hier_clust_stats

---
+++ R/hier_clust_stats.R
@@ -0,0 +1,11 @@
+#' Hierarchical (Agglomerative) Clustering via stats
+#'
+#' [hier_clust()] creates Hierarchical (Agglomerative) Clustering model.
+#'
+#' @includeRmd man/rmd/hier_clust_stats.md details
+#'
+#' @name details_hier_clust_stats
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed
