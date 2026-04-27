b0c402a806f66008a07dadfa46106d21c6855530
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-04-27 10:02:04 -0700

    test that tune::finalize_model() works

--- tests/testthat/test-finalize.R
+++ tests/testthat/test-finalize.R
@@ -31,6 +31,30 @@ test_that("finalize_model_tidyclust() errors on non-cluster_spec", {
   )
 })
 
+test_that("tune::finalize_model() works with cluster_spec", {
+  spec <- k_means(num_clusters = tune())
+  params <- data.frame(num_clusters = 5)
+
+  res <- tune::finalize_model(spec, params)
+
+  expect_s3_class(res, "cluster_spec")
+  expect_equal(res$args$num_clusters, new_empty_quosure(5))
+})
+
+test_that("tune::finalize_workflow() works with cluster_spec workflows", {
+  spec <- k_means(num_clusters = tune())
+  wf <- workflows::workflow() |>
+    workflows::add_model(spec) |>
+    workflows::add_formula(~.)
+  params <- data.frame(num_clusters = 4)
+
+  res <- tune::finalize_workflow(wf, params)
+
+  expect_s3_class(res, "workflow")
+  extracted_spec <- workflows::extract_spec_parsnip(res)
+  expect_equal(extracted_spec$args$num_clusters, new_empty_quosure(4))
+})
+
 test_that("finalize_model_tidyclust() is deprecated", {
   spec <- k_means(num_clusters = tune())
   params <- data.frame(num_clusters = 5)
