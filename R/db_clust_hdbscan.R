7ab321ef43d508fcbe274c073330eae00cab0973
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-05-12 12:21:29 -0700

    document hdbscan engine

---
+++ R/db_clust_hdbscan.R
@@ -0,0 +1,11 @@
+#' Hierarchical Density-Based Spatial Clustering (HDBSCAN) via dbscan
+#'
+#' [db_clust()] creates an HDBSCAN model.
+#'
+#' @includeRmd man/rmd/db_clust_hdbscan.md details
+#'
+#' @name details_db_clust_hdbscan
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed
